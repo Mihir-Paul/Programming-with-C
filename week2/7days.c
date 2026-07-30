1ce11b9a4ad99480dbd641f4f389590b0cb82cb7
Mihir Paul <mihirpgm@gmail.com>
2026-07-31 02:31:48 +0530

    Updated 7days

--- week2/7days.c
+++ week2/7days.c
@@ -5,12 +5,29 @@ int main(){
     scanf("%d",&number);
     switch(number){
         case 1:
-printf("Sunday");
-        break;
-        case 2:
            printf("Monday");
         break;
-
-           
-    }
+        case 2:
+           printf("Tuesday");
+        break;
+        case 3:
+            printf("Wednesday");
+        break;
+        case 4:
+             printf("Thursday");
+        break;
+        case 5:
+            printf("Friday");
+        break;
+        case 6:
+            printf("Saturday");
+        break;
+        case 7:
+            printf("Sunday");
+        break;
+        default:
+            printf("Invalid Day! \n");
+        
+        
+    }  return 0;
 }
